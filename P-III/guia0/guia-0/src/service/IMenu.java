3014c621cfd969caf6480fb8bc8ed126cbe66ea3
Matias Benedetti <benedettimaty@gmail.com>
2025-03-17 00:06:27 -0300

    agrega metodo buscarXId

--- P-III/guia0/guia-0/src/service/IMenu.java
+++ P-III/guia0/guia-0/src/service/IMenu.java
@@ -1,4 +1,13 @@
 package service;
 
 public interface IMenu {
+
+    void mostrarMenu();
+    void agregarMedia();
+    void eliminarMedia();
+    void mostrarLista();
+    void filtrarXTipo();
+    void filtrarXGenero();
+    void buscarXId();
+
 }
--- P-III/guia0/guia-0/src/service/Menu.java
+++ P-III/guia0/guia-0/src/service/Menu.java
@@ -1,4 +1,55 @@
 package service;
 
-public class Menu {
+import model.Media;
+import repository.Repositorio;
+
+import java.util.Scanner;
+
+public class Menu implements IMenu{
+
+    private Scanner scanner = new Scanner(System.in);
+    private Repositorio<Media> repositorio = new Repositorio<>();
+
+    @Override
+    public void mostrarMenu() {
+
+    }
+
+    @Override
+    public void agregarMedia() {
+
+    }
+
+    @Override
+    public void eliminarMedia() {
+
+    }
+
+    @Override
+    public void mostrarLista() {
+
+    }
+
+    @Override
+    public void filtrarXTipo() {
+
+    }
+
+    @Override
+    public void filtrarXGenero() {
+
+    }
+
+    @Override
+    public void buscarXId() {
+        System.out.println("Ingrese el ID que desea buscar: ");
+        String idABuscar = scanner.nextLine();
+        Media media = repositorio.buscarXId(idABuscar);
+
+        if (media != null){
+            System.out.println("Titulo: " + media.getTitulo());
+            System.out.println("\nCreador: " + media.getCreador());
+            System.out.println("\nGenero: " + media.getGenero());
+        }
+    }
 }
